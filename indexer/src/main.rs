1e9e7e9a5cf9da13cc0b0c1c7d0c044fdb975c84
Wesley Graham <wgraham@berkeley.edu>
2020-08-03 22:11:14 -0700

    Basic understanding & progress

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -1,3 +1,4 @@
+// Toy implementation based off the API defined here: https://docs.near.org/docs/interaction/rpc#transaction-status
 use std::env;
 use std::io;
 
@@ -7,14 +8,41 @@ use tokio::sync::mpsc;
 use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::EnvFilter;
 
+use near_sdk::{collections::{UnorderedMap,}}
+use near_primitives::{
+    receipt::Receipt,
+    transaction::{ExecutionOutcome, ExecutionStatus, SignedTransaction}
+};
+
+mod filter;
+
+let mut receiptMap: UnorderedMap<String, Receipt> = UnorderedMap::new(b"receipts".to_vec());
+
 use near_indexer;
 
 async fn listen_blocks(mut stream: mpsc::Receiver<near_indexer::BlockResponse>) {
     while let Some(block) = stream.recv().await {
         for chunk in block.chunks {
-            println!("txs {:?}", chunk.transactions);
-            println!("receipts {:?}", chunk.receipts);
+            // 1) Add receipts into hashMap
+            for receipt in chunk.receipts {
+                if filter::isSuccessfulReceipt(receipt) {
+                    receiptMap.insert(receipt.id, &receipt)
+                }
+            }
 
+            // 2) Iterate through searching for successful transactions
+            for transaction in chunk.transactions {
+                if filter::isFluxTransfer(transaction) {
+                    for receipt in transaction.transaction_outcomes {
+                        let receipt = receiptMap.get(&receipt).expect("receipt doesn't exist");
+                        // Means transaction is successful (as dictated by receiptMap)
+                        filter::processTransfer(receipt, transaction);
+                    }
+                };
+            }
+
+            //println!("txs {:?}", chunk.transactions);
+            //println!("receipts {:?}", chunk.receipts);
         }
     }
 }
