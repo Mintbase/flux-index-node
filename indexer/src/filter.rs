a2a3401a83299ac185d8b32ebcc79a524e0480b6
Wesley Graham <wgraham@berkeley.edu>
2020-08-09 12:59:27 -0700

    progress

--- indexer/src/filter.rs
+++ indexer/src/filter.rs
@@ -5,24 +5,30 @@ use near_primitives::{
     views,
     types
 };
-use near_indexer::streamer::streamer::Outcome;
+use near_indexer::Outcome;
 
 const FLUX_FUNGIBLE_RECEIVER_ID: String = "FluxFungibleContract".to_string();
 const FLUX_PROTOCOL_RECEIVER_ID: String = "FluxProtocolContract".to_string();
 
 
 pub fn isValidFluxTransfer(outcome: &Outcome ) -> bool {
-    match outcome {
-        Outcome::Receipt(views::ExecutionOutcomeWithIdView) => true,
+    let filtered_outcome: &Receipt = match outcome {
+        // Match a single value
+        Receipt => Receipt(outcome),
         _ => return false
     };
 
-    match outcome.status {
+    let final_outcome: &view::ExecutionOutcomeWithIdView = match filtered_outcome {
+        ExecutionOutcomeWithIdView => ExecutionOutcomeWithIdView(filtered_outcome),
+        _ => return false
+    };
+
+    match final_outcome.status {
         ExecutionStatus::SuccessValue(_) => true,
         _ => return false
     };
 
-    match outcome.outcome.receiver_id {
+    match final_outcome.outcome.receiver_id {
         FLUX_PROTOCOL_RECEIVER_ID => true,
         FLUX_FUNGIBLE_RECEIVER_ID => true,
         _ => return false
@@ -33,7 +39,7 @@ pub fn isValidFluxTransfer(outcome: &Outcome ) -> bool {
 // TODO: Return success value
 pub async fn processTransfer(outcome: &Outcome) -> bool {
     // Is the log going to be the best processing mechanism?
-    return match outcome.outcome.logs.r#type {
+    return match outcome.receipt.logs.r#type {
         "market_creation" => createMarketHandler(&outcome).await,
         "order_placed" => placeOrderHandler(&outcome).await,
         _ => false
